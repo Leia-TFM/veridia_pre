b0f1d0054b9efb169589ae7af301a4cc8121760d
Gonzalo Arcas Castrosin <gonzaloarcas222@gmail.com>
2026-03-19 13:23:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 from fastapi import FastAPI
-import os
-import sys
-# --- Ajuste de path para poder ejecutar tanto script como uvicorn (inamovible de esta posición) ---
-root_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-if root_path not in sys.path:
-    sys.path.append(root_path)
-
-from config import settings    #from api.config import settings
-from routes import analizar, traduccion #from api.routes import analizar, traduccion, estadisticas 
+from api.config import settings    #from api.config import settings
+from api.routes import analizar, traduccion #from api.routes import analizar, traduccion, estadisticas 
 
 app = FastAPI(
     title=settings.APP_NAME,
