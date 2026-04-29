19b98f70e33f33b91797eb4fe83ace71910c53be
Gonzalo Arcas Castrosin <gonzaloarcas222@gmail.com>
2026-04-29 12:40:12 +0200

    Update ocr_service.py

--- servicios/ocr_service.py
+++ servicios/ocr_service.py
@@ -3,8 +3,6 @@ from PIL import Image
 import cv2 
 import numpy as np 
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-
 # CARGAR IMAGEN
 def cargar_imagen(file_bytes):
     np_arr = np.frombuffer(file_bytes, np.uint8)
