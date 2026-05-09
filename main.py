f7e56af286699a8e7d1e6e1b69343e95beff25fc
Gonzalo Arcas Castrosin <gonzaloarcas222@gmail.com>
2026-05-09 13:40:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from api.config import settings
-from api.routes import analizar, traduccion #,estadisticas
+from api.routes import analizar, traduccion, estadisticas
 
 app = FastAPI(
     title=settings.APP_NAME,
@@ -11,7 +11,7 @@ app = FastAPI(
 # Incluir los routers de las rutas
 app.include_router(analizar.router, prefix="/api")
 app.include_router(traduccion.router, prefix="/api")
-#app.include_router(estadisticas.router, prefix="/api")
+app.include_router(estadisticas.router, prefix="/api")
 
 #Comprobamos que la aplicación se inicia correctamente
 @app.get("/")
