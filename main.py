f64d2c0e9f4c656bc99bfcd0c8a6574969e66e3c
Gonzalo Arcas Castrosin <gonzaloarcas222@gmail.com>
2026-05-10 18:04:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from api.config import settings
 from api.routes import analizar, traduccion, estadisticas
 
+
 app = FastAPI(
     title=settings.APP_NAME,
     version=settings.APP_VERSION,
@@ -9,13 +10,16 @@ app = FastAPI(
 )
 
 # Incluir los routers de las rutas
+app.include_router(estadisticas.router, prefix="/api")
 app.include_router(analizar.router, prefix="/api")
 app.include_router(traduccion.router, prefix="/api")
-app.include_router(estadisticas.router, prefix="/api")
 
+
+for route in app.routes:
+    print(route.path)
 #Comprobamos que la aplicación se inicia correctamente
 @app.get("/")
 async def health_check():
     return {"estado":"activo", "app":settings.APP_NAME}
 
-#Ejecución: uvicorn main:app --reload
+#Ejecución: uvicorn main:app
